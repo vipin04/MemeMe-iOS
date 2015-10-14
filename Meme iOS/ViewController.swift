22caf62f206486f919d976fe03ffcb943ba50b82
Vipin Aggarwal <vipinaggarwal04@gmail.com>
2015-10-14 08:54:09 +0530

    Initial commit

--- Meme iOS/ViewController.swift
+++ Meme iOS/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func AlbumButtonTapped(sender: AnyObject) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = pickedImage
+            imageView.contentMode = .ScaleToFill
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true;
+    }
+    
+    @IBAction func albumButtonaTapped(sender: AnyObject) {
+        let imagePicker = UIImagePickerController();
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
 
-
+        imagePicker.delegate = self
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
