cd5b6d0f6068db3879f3581e925c5f25d6d3860b
Vipin Aggarwal <vipinaggarwal04@gmail.com>
2015-10-17 10:32:13 +0530

    Moving keyboard if it overlaps with textfield (or any view that brings up the keyboard)

--- Meme iOS/ViewController.swift
+++ Meme iOS/ViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate{
 
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    
+    var isViewRepositioned = false
     
     @IBAction func AlbumButtonTapped(sender: AnyObject) {
         
@@ -20,6 +25,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        self.viewWillDisappear(animated)
+        self.unsubscribeFromKeyboardNotifications()
+
     }
 
 
@@ -50,5 +66,73 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         self.presentViewController(imagePicker, animated: true, completion: nil)
     }
     
+    
+
+    
+    //Register for Notifications when keyboard appears
+    func subscribeToKeyboardNotifications () {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications () {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+    }
+    
+    //Move the view (if required) when keyboard is shown
+    func keyboardWillShow (notification:NSNotification) {
+        let firstResponder = getFirstResponder()
+        
+        //Get y coordinate of full blown keyboard
+        let keyboardY = getKeyboardRect(notification).origin.y - getKeyboardHeight(notification)
+        
+        if firstResponder.frame.origin.y >= keyboardY {
+            self.view.frame.origin.y -= getKeyboardHeight(notification)
+            isViewRepositioned = true
+        }
+    }
+    
+    //Move the view (if required) when keyboard is hidden
+    func keyboardWillHide (notification:NSNotification) {
+        if isViewRepositioned {
+            self.view.frame.origin.y += getKeyboardHeight(notification)
+            isViewRepositioned = false
+        }
+    }
+    
+    
+    
+    func getKeyboardHeight(notification:NSNotification) -> CGFloat    {
+        return getKeyboardRect(notification).height
+    }
+    
+    
+    func getKeyboardRect(notification: NSNotification) -> CGRect {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue
+        return keyboardSize.CGRectValue();
+    }
+    
+    
+    
+    //Returns the first responder object
+    func getFirstResponder () -> UIView {
+        for view in self.view.subviews {
+            if view.isFirstResponder() {
+                return view
+            }
+        }
+        
+        return self.view  //If none of the subviews are first responder, return self.view
+    }
+    
+    
+    
+    
+    //UITextField Delegates
+    func textFieldShouldReturn (textField:UITextField) -> Bool{
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
